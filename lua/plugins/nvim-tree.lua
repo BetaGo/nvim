65defe792b5e4f3842ea1ef2082b92121dc6c2bd
BetaGo <gouwantong@live.com>
2023-01-14 16:34:05 +0800

    update

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,15 +1,18 @@
 require("nvim-tree").setup({
-
 	view = {
 		side = "right",
 	},
-    renderer = {
-        indent_markers = {
-            enable = true
-        }
-    },
-    diagnostics = {
-        enable = true
-    }
-})
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
 
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
+	},
+	diagnostics = {
+		enable = true,
+	},
+})
