55454d5e641f586d7e55f936ebfab55d57a2c62f
BetaGo <gouwantong@live.com>
2023-02-19 09:52:23 +0800

    update

--- lua/custom-theme.lua
+++ lua/custom-theme.lua
@@ -1,6 +1,6 @@
 --Set colorscheme (order is important here)
 vim.o.termguicolors = true
-vim.o.background = "dark"
+-- vim.o.background = "dark"
 -- vim.o.background = "light"
 -- vim.g.transparent_enabled = true;
 
