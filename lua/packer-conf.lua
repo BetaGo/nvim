e03a245cc9ed0f5deecebad931350e2917064981
WanTong <gouwantong@live.com>
2023-06-02 11:18:50 +0800

    Update packer-conf.lua

--- lua/packer-conf.lua
+++ lua/packer-conf.lua
@@ -10,23 +10,23 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 local default_url_format = vim.g.download_plugin_use_ssh and "git@github.com:%s" or "https://github.com/%s"
-local not_in_vscode = true
-if vim.g.vscode then
-	not_in_vscode = false
+
+local nocode = function()
+  return vim.fn.exists('g:vscode') == 0
 end
 
 local use = require("packer").use
 require("packer").startup({
 	function()
 		use("wbthomason/packer.nvim") -- Package manager
-		use({ "kdheepak/lazygit.nvim", cond = not_in_vscode })
-		use("tpope/vim-commentary") -- "gc" to comment visual regions/lines
-		use({ "JoosepAlviste/nvim-ts-context-commentstring", cond = not_in_vscode }) -- jsx comment support
+		use({ "kdheepak/lazygit.nvim", cond = { nocode } })
+		use({"tpope/vim-commentary", cond = { nocode }}) -- "gc" to comment visual regions/lines
+		use({ "JoosepAlviste/nvim-ts-context-commentstring", cond = { nocode } }) -- jsx comment support
 		-- UI to select things (files, grep results, open buffers...)
 		use({
 			"nvim-telescope/telescope.nvim",
 			requires = { "nvim-lua/plenary.nvim" },
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.telescope")
 			end,
@@ -35,21 +35,23 @@ require("packer").startup({
 			"sainnhe/edge",
 			-- "shaunsingh/nord.nvim",
 			-- "arcticicestudio/nord-vim",
+			cond = { nocode },
 			config = function()
 				require("custom-theme")
 			end,
 		}) -- theme packages
 		-- Add indentation guides even on blank lines
-		use({ "lukas-reineke/indent-blankline.nvim", cond = not_in_vscode })
+		use({ "lukas-reineke/indent-blankline.nvim", cond = { nocode } })
 		-- Highlight, edit, and navigate code using a fast incremental parsing library
 		use({
 			"nvim-treesitter/nvim-treesitter",
+			cond = { nocode },
 			config = function()
 				require("plugins.nvim-treesitter")
 			end,
 		})
 		-- Additional textobjects for treesitter
-		use({ "nvim-treesitter/nvim-treesitter-textobjects" })
+		use({ "nvim-treesitter/nvim-treesitter-textobjects" , cond = { nocode }})
 		use({
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
@@ -58,11 +60,12 @@ require("packer").startup({
 			"hrsh7th/cmp-nvim-lsp",
 			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip",
+			cond = { nocode }
 		}) -- Collection of configurations for built-in LSP client
 		use({
 			"kyazdani42/nvim-tree.lua",
 			requires = { "kyazdani42/nvim-web-devicons" },
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.nvim-tree")
 			end,
@@ -71,38 +74,38 @@ require("packer").startup({
 			"akinsho/bufferline.nvim",
 			tag = "v3.*",
 			requires = "kyazdani42/nvim-web-devicons",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.bufferline")
 			end,
 		})
-		use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim", cond = not_in_vscode })
-		use({ "kassio/neoterm", cond = not_in_vscode })
+		use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim", cond = { nocode } })
+		use({ "kassio/neoterm", cond = { nocode } })
 		use({
 			"sbdchd/neoformat",
-			cond = not_in_vscode,
+			-- cond = { nocode },
 			config = function()
 				require("plugins.neoformat")
 			end,
 		})
 		use({
 			"nvim-lualine/lualine.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.lualine")
 			end,
 		})
 		use({
 			"simrat39/symbols-outline.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.symbols-outline")
 			end,
 		})
-		use({ "stevearc/dressing.nvim", cond = not_in_vscode })
+		use({ "stevearc/dressing.nvim", cond = { nocode } })
 		use({
 			"folke/which-key.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.which-key")
 			end,
@@ -110,7 +113,7 @@ require("packer").startup({
 		use({ "lambdalisue/suda.vim" }) -- open & write file as sudo
 		use({
 			"rcarriga/nvim-notify",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.nvim-notify")
 			end,
@@ -118,15 +121,15 @@ require("packer").startup({
 		use({
 			"goolord/alpha-nvim",
 			requires = { "kyazdani42/nvim-web-devicons" },
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.alpha")
 			end,
 		}) -- greeter for neovim
-		use({ "jmcantrell/vim-diffchanges", cond = not_in_vscode }) -- diff local changes
+		use({ "jmcantrell/vim-diffchanges", cond = { nocode } }) -- diff local changes
 		use({
 			"anuvyklack/pretty-fold.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.pretty-fold")
 			end,
@@ -134,7 +137,7 @@ require("packer").startup({
 		use({
 			"folke/todo-comments.nvim",
 			requires = "nvim-lua/plenary.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.todo-comments")
 			end,
@@ -142,26 +145,26 @@ require("packer").startup({
 		use({
 			"folke/trouble.nvim",
 			requires = "kyazdani42/nvim-web-devicons",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.trouble")
 			end,
 		}) -- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists to help you solve all the trouble your code is causing.
 		use({
 			"nmac427/guess-indent.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.guess-indent")
 			end,
 		})
 		use({
 			"lewis6991/spellsitter.nvim",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.spellsitter")
 			end,
 		})
-		use({ "ap/vim-css-color", cond = not_in_vscode })
+		use({ "ap/vim-css-color", cond = { nocode } })
 		use({
 			"unblevable/quick-scope",
 			config = function()
@@ -170,7 +173,7 @@ require("packer").startup({
 		})
 		use({
 			"BetaGo/nvim-colorizer.lua",
-			cond = not_in_vscode,
+			cond = { nocode },
 			config = function()
 				require("plugins.nvim-colorizer")
 			end,
