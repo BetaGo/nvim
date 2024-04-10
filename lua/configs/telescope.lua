0c4cb84c6da4de34519d117e9bf976735e4e940c
BetaGo <gouwantong@live.com>
2024-04-10 22:37:17 +0800

    update

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -48,196 +48,6 @@ end
 -- Telescope
 require("telescope").setup({
 	defaults = {
-		create_layout = function(picker)
-			local border = {
-				results = {
-					top_left = "┌",
-					top = "─",
-					top_right = "┬",
-					right = "│",
-					bottom_right = "",
-					bottom = "",
-					bottom_left = "",
-					left = "│",
-				},
-				results_patch = {
-					minimal = {
-						top_left = "┌",
-						top_right = "┐",
-					},
-					horizontal = {
-						top_left = "┌",
-						top_right = "┬",
-					},
-					vertical = {
-						top_left = "├",
-						top_right = "┤",
-					},
-				},
-				prompt = {
-					top_left = "├",
-					top = "─",
-					top_right = "┤",
-					right = "│",
-					bottom_right = "┘",
-					bottom = "─",
-					bottom_left = "└",
-					left = "│",
-				},
-				prompt_patch = {
-					minimal = {
-						bottom_right = "┘",
-					},
-					horizontal = {
-						bottom_right = "┴",
-					},
-					vertical = {
-						bottom_right = "┘",
-					},
-				},
-				preview = {
-					top_left = "┌",
-					top = "─",
-					top_right = "┐",
-					right = "│",
-					bottom_right = "┘",
-					bottom = "─",
-					bottom_left = "└",
-					left = "│",
-				},
-				preview_patch = {
-					minimal = {},
-					horizontal = {
-						bottom = "─",
-						bottom_left = "",
-						bottom_right = "┘",
-						left = "",
-						top_left = "",
-					},
-					vertical = {
-						bottom = "",
-						bottom_left = "",
-						bottom_right = "",
-						left = "│",
-						top_left = "┌",
-					},
-				},
-			}
-
-			local results = make_popup({
-				focusable = false,
-				border = {
-					style = border.results,
-					text = {
-						top = picker.results_title,
-						top_align = "center",
-					},
-				},
-				win_options = {
-					winhighlight = "Normal:Normal",
-				},
-			})
-
-			local prompt = make_popup({
-				enter = true,
-				border = {
-					style = border.prompt,
-					text = {
-						top = picker.prompt_title,
-						top_align = "center",
-					},
-				},
-				win_options = {
-					winhighlight = "Normal:Normal",
-				},
-			})
-
-			local preview = make_popup({
-				focusable = false,
-				border = {
-					style = border.preview,
-					text = {
-						top = picker.preview_title,
-						top_align = "center",
-					},
-				},
-			})
-
-			local box_by_kind = {
-				vertical = Layout.Box({
-					Layout.Box(preview, { grow = 1 }),
-					Layout.Box(results, { grow = 1 }),
-					Layout.Box(prompt, { size = 3 }),
-				}, { dir = "col" }),
-				horizontal = Layout.Box({
-					Layout.Box({
-						Layout.Box(results, { grow = 1 }),
-						Layout.Box(prompt, { size = 3 }),
-					}, { dir = "col", size = "50%" }),
-					Layout.Box(preview, { size = "50%" }),
-				}, { dir = "row" }),
-				minimal = Layout.Box({
-					Layout.Box(results, { grow = 1 }),
-					Layout.Box(prompt, { size = 3 }),
-				}, { dir = "col" }),
-			}
-
-			local function get_box()
-				local strategy = picker.layout_strategy
-				if strategy == "vertical" or strategy == "horizontal" then
-					return box_by_kind[strategy], strategy
-				end
-
-				local height, width = vim.o.lines, vim.o.columns
-				local box_kind = "horizontal"
-				if width < 100 then
-					box_kind = "vertical"
-					if height < 40 then
-						box_kind = "minimal"
-					end
-				end
-				return box_by_kind[box_kind], box_kind
-			end
-
-			local function prepare_layout_parts(layout, box_type)
-				layout.results = results
-				results.border:set_style(border.results_patch[box_type])
-
-				layout.prompt = prompt
-				prompt.border:set_style(border.prompt_patch[box_type])
-
-				if box_type == "minimal" then
-					layout.preview = nil
-				else
-					layout.preview = preview
-					preview.border:set_style(border.preview_patch[box_type])
-				end
-			end
-
-			local function get_layout_size(box_kind)
-				return picker.layout_config[box_kind == "minimal" and "vertical" or box_kind].size
-			end
-
-			local box, box_kind = get_box()
-			local layout = Layout({
-				relative = "editor",
-				position = "50%",
-				size = get_layout_size(box_kind),
-			}, box)
-
-			layout.picker = picker
-			prepare_layout_parts(layout, box_kind)
-
-			local layout_update = layout.update
-			function layout:update()
-				local box, box_kind = get_box()
-				prepare_layout_parts(layout, box_kind)
-				layout_update(self, { size = get_layout_size(box_kind) }, box)
-			end
-
-			return TSLayout(layout)
-		end,
-		buffer_previewer_maker = new_maker, -- Ignore files bigger than a threshold
 		mappings = {
 			i = {
 				["<C-y>"] = copy_selected_entry,
@@ -250,37 +60,32 @@ require("telescope").setup({
 				["<C-c>"] = actions.close,
 			},
 		},
-		-- sorting_strategy = "ascending",
-		layout_strategy = "flex",
+
+		prompt_prefix = "   ",
+		selection_caret = "  ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
 		layout_config = {
 			horizontal = {
-				size = {
-					width = "90%",
-					height = "60%",
-				},
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
 			},
 			vertical = {
-				size = {
-					width = "90%",
-					height = "90%",
-				},
+				mirror = false,
 			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
 		},
-
-		-- layout_strategy = "horizontal",
-		-- layout_config = {
-		-- 	horizontal = {
-		-- 		prompt_position = "top",
-		-- 		preview_width = 0.55,
-		-- 		results_width = 0.8,
-		-- 	},
-		-- 	vertical = {
-		-- 		mirror = false,
-		-- 	},
-		-- 	width = 0.87,
-		-- 	height = 0.80,
-		-- 	preview_cutoff = 120,
-		-- },
+		winblend = 0,
+		border = {},
+		borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+		color_devicons = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
 		preview = {
 			mime_hook = function(filepath, bufnr, opts)
 				local is_image = function(filepath)
